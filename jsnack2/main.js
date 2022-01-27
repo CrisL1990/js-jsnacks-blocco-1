0eb929952b496a5b797190da3292c6c99b4582b9
CrisL1990 <lucchetta.cris@gmail.com>
2022-01-27 14:57:47 +0100

    Inizio esercizi

---
+++ jsnack2/main.js
@@ -0,0 +1,14 @@
+//Definisce quanti numeri sono richiesti in input dall'utente
+let numbers = 10;
+
+//Definisce una lista all'interno della quale saranno immagazzinati i numeri in input
+numberList = [];
+
+for(let i = 0; i <= numbers; i++){
+
+    number = parseInt(prompt("Inserisci un numero: "));
+    numberList.push(number);
+
+    
+
+}
