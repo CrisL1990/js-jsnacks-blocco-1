f9c5156933ae760dca0f7a6747aea023a2de55ae
CrisL1990 <lucchetta.cris@gmail.com>
2022-01-27 15:15:02 +0100

    Esercizio 1 completato

---
+++ jsnack1/main.js
@@ -0,0 +1,33 @@
+//Definisce quanti numeri sono richiesti in input dall'utente
+let numbers = 3;
+
+//Definisce una lista all'interno della quale saranno immagazzinati i numeri in input
+numberList = [];
+
+//Immagazzina somma dei numeri presenti nella lista
+let total = 0;
+
+//Chiede numeri all'utente e li inserisce nella lista
+for(let i = 0; i < numbers; i++){
+    
+    number = parseInt(prompt("Inserisci un numero: "));
+
+    //Verifica che il numero inserito sia un numero e non sia inferiore a 0
+    if(number < 0 || isNaN(number)){
+        alert("Per favore inserisci un numero valido")
+        i--;
+    }
+
+    else{   
+        numberList.push(number);
+    }
+    
+}
+
+//Esegue la somma dei numeri presenti nella lista
+for(let i = 0; i<numberList.length; i++){
+    total += numberList[i];
+}
+
+//Stampa somma numeri
+alert("La somma dei numeri inseriti è: " + total);
